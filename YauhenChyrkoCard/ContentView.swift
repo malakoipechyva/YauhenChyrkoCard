b4f9b8a61379cd744cd495cdc12f7582a6b3d66e
Yauhen  Chyrko <chirko.eugene@gmail.com>
2020-11-25 18:42:08 +0300

    add name and photo

--- YauhenChyrkoCard/ContentView.swift
+++ YauhenChyrkoCard/ContentView.swift
@@ -9,8 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      ZStack {
+        Color(red: 0.17, green: 0.24, blue: 0.31, opacity: 1.00)
+          .ignoresSafeArea(.all)
+        VStack {
+          Image("cv_yauhen_chyrko")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 200, height: 200)
+            .clipShape(Circle())
+            .overlay(
+              Circle().stroke(Color.white, lineWidth: 5)
+            )
+          Text("Yauhen Chyrko")
+            .font(Font.custom("OpenSans-Regular", size: 40))
+            .bold()
+            .foregroundColor(.white)
+          Text("iOS Developer")
+            .foregroundColor(.white)
+            .font(.system(size: 25))
+        }
+      }
     }
 }
 
