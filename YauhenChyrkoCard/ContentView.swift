8088ecd5bc4315cfc54d9aae5c641872ba0f76e5
Yauhen  Chyrko <chirko.eugene@gmail.com>
2020-11-25 12:59:56 +0300

    initial commit

---
+++ YauhenChyrkoCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YauhenChyrkoCard
+//
+//  Created by malakoipechyva on 25.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YauhenChyrkoCard/YauhenChyrkoCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  YauhenChyrkoCardApp.swift
+//  YauhenChyrkoCard
+//
+//  Created by malakoipechyva on 25.11.20.
+//
+
+import SwiftUI
+
+@main
+struct YauhenChyrkoCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
